9197fdcd213f4630e49e0043a3b46cc35ac05d35
Ubuntu <ubuntu@ip-172-31-3-11.us-east-2.compute.internal>
2019-01-05 21:15:35 +0000

    Deleted

--- HLF/channel.sh
+++ HLF/channel.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 export GENESIS_BLOCK=/etc/crypto-config/Orci-Care-Kubernetes/HLF/channel-artifacts/orderer-channel.tx
 export POD_ID=`kubectl get pod | grep peer0 | cut -f1 -d' '`
-export ORDERER_ADDR="10.100.2.76"
+export ORDERER_ADDR="10.100.71.168"
 export ORG_DOMAIN="org1.example.com"
 export CORE_PEER_LOCALMSPID="Org1MSP"
+export ORDERER_MSPID="OrdererMSP"
 
 echo "POD_ID: " $POD_ID
 
-kubectl exec $POD_ID -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/Orci-Care-Kubernetes/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID peer channel create -o $ORDERER_ADDR:7050 -c mychannel -f $GENESIS_BLOCK"
+kubectl exec $POD_ID -it -- bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/Orci-Care-Kubernetes/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && peer channel create -o $ORDERER_ADDR:7050 -c mychannel -f $GENESIS_BLOCK"
 
-#kubectl exec $POD_ID -it /bin/bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/Orci-Care-Kubernetes/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
+kubectl exec $POD_ID -it /bin/bash -c "CORE_PEER_MSPCONFIGPATH=/etc/crypto-config/Orci-Care-Kubernetes/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp && CORE_PEER_ADDRESS=0.0.0.0:7051 && CORE_PEER_LOCALMSPID=$CORE_PEER_LOCALMSPID && peer channel join -b mychannel.block -o $ORDERER_ADDR:7050"
