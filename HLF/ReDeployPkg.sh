f2e7180778ee65d48660fe50010aa4ef709c5bab
Ubuntu <ubuntu@ip-172-31-3-11.us-east-2.compute.internal>
2019-01-06 17:24:02 +0000

    Deleted

--- HLF/ReDeployPkg.sh
+++ HLF/ReDeployPkg.sh
@@ -1,4 +1,4 @@
-ORDERER_PVC_ID=`kubectl get pvc | grep orderer | cut -f1 -d' '`
+export ORDERER_PVC_ID=`kubectl get pvc | grep orderer | cut -f1 -d' '`
 kubectl delete pvc $ORDERER_PVC_ID
 
 echo "***** PVC LIST ******"
