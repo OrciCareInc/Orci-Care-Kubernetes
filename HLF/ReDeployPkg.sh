91ebab634207ee4572692aa757ecdb00a921aab6
Ubuntu <ubuntu@ip-172-31-3-11.us-east-2.compute.internal>
2019-01-06 19:57:48 +0000

    Deleted

--- HLF/ReDeployPkg.sh
+++ HLF/ReDeployPkg.sh
@@ -1,3 +1,9 @@
+helm delete test
+
+helm del --purge test
+
+sleep 2
+
 export ORDERER_PVC_ID=`kubectl get pvc | grep orderer | cut -f1 -d' '`
 kubectl delete pvc $ORDERER_PVC_ID
 
@@ -7,10 +13,6 @@ kubectl get pvc
 echo "***** PV LIST ******"
 kubectl get pv
 
-helm delete test 
-
-helm del --purge test
-
-sleep 5
+sleep 2
 
 helm install --name test ./fabric-artifacts -f org1.example.com.yaml
